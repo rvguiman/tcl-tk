7c4f86254642ae0defb88dfc9961219f73d63c43
Ricardo Victor Guiman II <rvguiman@gmail.com>
2014-06-17 08:55:50 +0800

    Create fam_silk_icon.tcl

---
+++ sample_gui/fam_silk_icon.tcl
@@ -0,0 +1,24 @@
+#!/bin/sh
+# next line starts tclsh
+exec /bin/wish
+
+#-------------------------------------------------------------------#
+# what: sample using famfamfam icons and package from famfamfam.com
+# author: ricardo
+#-------------------------------------------------------------------#
+
+# append your auto path with the package directory
+# lappand ::auto_path /tcl/lib/your_dir/famfamfam1/
+# lappand ::auto_path /tcl/lib/your_dir/famfamfam_silk1/
+
+package require famfamfam
+package require famfamfam::silk
+
+proc iniGui { } {
+  #get image icon, get image name from "http://famfamfam.com/lab/icons/silk/previews/index_abc.png"
+  set exitIcon [::famfamfam::silk get door_out]
+  button .buttQuit -text "Quit" -command exit -image $exitIcon -compound left
+  pack .buttQuit
+}
+
+iniGui
